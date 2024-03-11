fba7f6fefc192f4104f7ba9c955861f1669073e1
AndresSamboni <andres.pag.19@gmail.com>
2024-03-10 19:03:47 -0500

    feat: Deactivate the copy, paste and cut events.

--- work1_IPValidate/src/js/challengue_js.js
+++ work1_IPValidate/src/js/challengue_js.js
@@ -13,6 +13,13 @@ let realValue = 0;
 let myForm = document.getElementById('myForm');
 let ipAdd = document.getElementById('ipAdd');
 
+// DEACTIVATE THE PASTE EVENT
+ipAdd.addEventListener('paste', (e) =>{e.preventDefault();});
+// DEACTIVATE THE COPY EVENT
+ipAdd.addEventListener('copy', (e) =>{e.preventDefault();});
+// DEACTIVATE THE CUT EVENT
+ipAdd.addEventListener('cut', (e) =>{e.preventDefault();});
+
 // ACTIVATE THE KEYPRESS EVENT
 ipAdd.addEventListener('keypress', (e) =>{
     // 10. Prevent that other characters that aren't the dot and numbers are used.
