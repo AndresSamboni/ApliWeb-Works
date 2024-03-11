8616b957d4f6323af42eb7b18a4f7d5cef66a827
AndresSamboni <andres.pag.19@gmail.com>
2024-03-10 19:29:52 -0500

    feat: Create the function to display warnings and the condition to the IP limit.

--- work1_IPValidate/src/js/challengue_js.js
+++ work1_IPValidate/src/js/challengue_js.js
@@ -24,8 +24,45 @@ ipAdd.addEventListener('cut', (e) =>{e.preventDefault();});
 ipAdd.addEventListener('keypress', (e) =>{
     // 10. Prevent that other characters that aren't the dot and numbers are used.
     if(/\d|\./.test(e.key)){
-
+        // 2. Limit the Ipv4
+        if(ipAdd.value.length == maxLenInput || ipLimit){
+            e.preventDefault();
+            showWarn('maxIp');
+        }else{
+            showWarn('maxIp',`Para completar la dirección restan ${maxLenInput-ipAdd.value.length} caracteres.`);
+        }
     }else{
         e.preventDefault();
     }
 });
+
+// CREATE A FUNCTION THAT SHOW A WARNING.
+function showWarn(idWarn,warnMessage){
+    // Check if the warning is displayed and delete.
+    const existWarn = document.querySelector(`#myForm #${idWarn}`);
+    if(existWarn){
+        existWarn.remove();
+    }
+    // Check if a message exists.
+    if(warnMessage){
+        // Create a new span element.
+        let newSpanWarn=document.createElement('span');
+        // Put the identifier to the warning message.
+        newSpanWarn.id=idWarn;
+        // Put the message style.
+        newSpanWarn.style.marginInline='5%';
+        newSpanWarn.style.marginTop='1%';
+        newSpanWarn.style.display='block';
+        newSpanWarn.style.fontFamily='Times New Roman';
+        newSpanWarn.style.fontSize='1.5vw';
+        newSpanWarn.style.maxWidth='100%';
+        newSpanWarn.style.color='rgb(144,144,52)';
+        // Put the warning message.
+        newSpanWarn.innerHTML=warnMessage;
+        // Put the span element into the form element.
+        myForm.appendChild(newSpanWarn);
+    }
+}
+
+// Call the showWarn function to present the initial warning.
+showWarn ('maxIp','Las direcciones IP tienen un total de 15 caracteres');
