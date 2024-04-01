1b4c73826283e5caf16ae3c95438b0d40605f69d
AndresSamboni <andres.pag.19@gmail.com>
2024-03-31 23:54:27 -0500

    feat: Barra de navegación adaptativa, util en PC y teléfonos.

--- Apliweb_v2/src/app/components/root/root.component.ts
+++ Apliweb_v2/src/app/components/root/root.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { InformationComponent } from '../information/information.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    InformationComponent
+  ],
   templateUrl: './root.component.html',
   styleUrl: './root.component.css'
 })
 export class RootComponent {
+  // ATTRIBUTES
+  public screenWidth: number;
+  // CONSTRUCTOR
+  constructor() {
+    this.screenWidth = window.innerWidth;
+  }
 
+  // Method launched right after the constructor
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+    };
+  }
 }
